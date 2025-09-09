dfadc64afaadc8ff83a0a87c036139ec53e31278
michivonah <info@michivonah.ch>
2025-09-09 21:13:02 +0200

    fix db client so that casing works

--- api/src/db/client.ts
+++ api/src/db/client.ts
@@ -1,9 +1,8 @@
 import { drizzle } from 'drizzle-orm/d1';
 import type { Context } from 'hono';
 
-export function dbConnection(c: Context){
-    return drizzle({
-        connection: c.env.db,
+export function dbConn(c: Context){
+    return drizzle(c.env.d1_db, {
         casing: 'snake_case'
     })
 }
